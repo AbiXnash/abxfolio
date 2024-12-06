79d203a2eb91c170f162e4967da65bed4ad822ca
AbiXnash <abixnash@outlook.com>
2024-12-06 23:18:15 +0530

    feat: add achievements list to Achievements page

--- src/pages/Achievements/Achievements.tsx
+++ src/pages/Achievements/Achievements.tsx
@@ -1,7 +1,13 @@
 const Achievements = () => {
 	return <>
 		<h1>Achievements</h1>
-
+		<ul>
+			<li>Presented our Reasearch paper at the <strong>4th International Conference on Computing, Analytics, and Networks (ICAN 2024)</strong>, jointly organized by Chitkara University</li>
+			<li>Presented our research paper at the <strong>14th International Conference on Applications and Technologies in Information Security (ATIS 2024)</strong>, hosted by SASTRA Deemed to be University</li>
+			<li>Completed 2+ years of coursework in the Access Microscholarship Program, sponsored by <strong>U. S. Department of State</strong></li>
+			<li>Author of the book titled <q>Fleeting Fables: A Symphony of Flash Fiction</q></li>
+			<li>Organized a workshop on MERN Stack, <q>Indepth Look into MERN Stack</q> under the banner of SASTRA Free &amp; Open Source Software (SFOSS)</li>
+		</ul>
 	</>
 }
 
