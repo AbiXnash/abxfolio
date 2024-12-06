5c43cc3157786ecdffc8a2a36b505379ae8c62fe
AbiXnash <abixnash@outlook.com>
2024-12-07 00:01:16 +0530

    feat: implement smooth scroll to top button with visibility toggle and add triangle icon

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,37 @@
-import './Footer.css'
+import { useState, useEffect } from 'react';
+import './Footer.css';
+import triangle from '../../assets/triangle.svg';
 
 const Footer = () => {
-	return <>
+	const [isVisible, setIsVisible] = useState(false);
+
+	const toggleVisibility = () => {
+		if (window.scrollY > 300) {
+			setIsVisible(true);
+		} else {
+			setIsVisible(false);
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener('scroll', toggleVisibility);
+		return () => window.removeEventListener('scroll', toggleVisibility);
+	}, []);
+
+	return (
 		<footer>
 			<div>
 				All copyrights reserved &copy; | Abinash {new Date().getFullYear()}
 			</div>
 			<div>
-				<button className="top-btn" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>^</button>
+				{isVisible && (
+					<button className="top-btn" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+						<img className="triangle" src={triangle} alt="Top" />
+					</button>
+				)}
 			</div>
 		</footer>
-	</>
-}
+	);
+};
 
-export default Footer
+export default Footer;
