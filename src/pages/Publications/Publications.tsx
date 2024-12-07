b3039ab4eaba72eb24ee0f3688fd120a609ce997
AbiXnash <abixnash@outlook.com>
2024-12-07 20:58:44 +0530

    feat: enhance styling in App.css with new color variables and improve Publications component with status-based styling

--- src/pages/Publications/Publications.tsx
+++ src/pages/Publications/Publications.tsx
@@ -8,42 +8,55 @@ const Publications = () => {
 		year: string;
 		publisher: string;
 		status: ["Published" | "Accepted" | "Submitted" | "In Preparation"];
-    link: string | null;
+		link: string | null;
 	}[] = [
-			{
-				id: 1,
-				title: "HATT-MLPNN: A Hybrid Approach for Cyber-Attack Detection in Industrial Control Systems Using MLPNN and Attention Mechanisms",
-				first_author: "Abinash S",
-				authors: ["Shankar Sriram V S", "Kannan Krithivasan"],
-				corresponding_author: "Priyanga S",
+		{
+			id: 1,
+			title: "HATT-MLPNN: A Hybrid Approach for Cyber-Attack Detection in Industrial Control Systems Using MLPNN and Attention Mechanisms",
+			first_author: "Abinash S",
+			authors: ["Shankar Sriram V S", "Kannan Krithivasan"],
+			corresponding_author: "Priyanga S",
 			year: "2024",
-				publisher: "International Conference on Applications and Technologies in Information Security",
-				status: ["Published"],
-				link: "https://example.com"
-			},
-			{
-				id: 2,
-				title: "HGCNN-LSTM: A Data-Driven Approach for Cyber Attack Detection in Cyber-Physical Systems",
-				first_author: "Abinash S",
-				authors: ["Srivatvan N", "Hemachandran S K"],
-				corresponding_author: "Priyanga S",
-				year: "2024",
-				publisher: "2024",
-				status: ["Accepted"],
-				link: null
-			},
-			{
-				id: 3,
-				title: "HyperCoar-DAM: A Robust Intrusion Detection System for Enhancing IIoT Networks Protection",
-				first_author: "Priyanga S",
-				authors: ["Abinash S", "Srivatvan N", "Hemachandran S K", "Kanan Krithivasan"],
-				corresponding_author: "Shankar Sriram V S",
-				year: "2024",
-				publisher: "International Journal of Advances in Engineering and Applied Mathematics",
-				status: ["Accepted"],
-				link: null
-			}
-	]
+			publisher: "International Conference on Applications and Technologies in Information Security",
+			status: ["Published"],
+			link: "https://example.com"
+		},
+		{
+			id: 2,
+			title: "HGCNN-LSTM: A Data-Driven Approach for Cyber Attack Detection in Cyber-Physical Systems",
+			first_author: "Abinash S",
+			authors: ["Srivatvan N", "Hemachandran S K"],
+			corresponding_author: "Priyanga S",
+			year: "2024",
+			publisher: "2024",
+			status: ["Accepted"],
+			link: null
+		},
+		{
+			id: 3,
+			title: "HyperCoar-DAM: A Robust Intrusion Detection System for Enhancing IIoT Networks Protection",
+			first_author: "Priyanga S",
+			authors: ["Abinash S", "Srivatvan N", "Hemachandran S K", "Kanan Krithivasan"],
+			corresponding_author: "Shankar Sriram V S",
+			year: "2024",
+			publisher: "International Journal of Advances in Engineering and Applied Mathematics",
+			status: ["Accepted"],
+			link: null
+		}
+	];
+
+	const getStatusClass = (status: string) => {
+		switch (status) {
+			case "Published":
+				return "green";
+			case "Accepted":
+			case "Submitted":
+				return "blue";
+			default:
+				return "grey";
+		}
+	};
+
 	return <>
 		<h1>Publications</h1>
 		<table>
@@ -52,17 +65,17 @@ const Publications = () => {
 					return <tr key={index}>
 						<td>[<em>{publication.id}</em>]</td>
 						<td>
-							{ publication.first_author.includes("Abinash") ? <strong>{publication.first_author}</strong> : publication.first_author },&nbsp;
+							{publication.first_author.includes("Abinash") ? <strong>{publication.first_author}</strong> : publication.first_author},&nbsp;
 							{
 								publication.authors.map((author, key) => {
 									return <span key={key}>{author.includes("Abinash") ? <strong>{author}</strong> : author}, </span>
 								})
 							}
-							<em>{ publication.title }, </em>
-							{ publication.publisher }, &nbsp;
-							{ publication.year }. &nbsp;
-							<span>{ publication.status }</span>
-							{ publication.link && <a href={publication.link} target="_blank" rel="noopener noreferrer"> [Link] </a> }
+							<em>{publication.title}, </em>
+							{publication.publisher}, &nbsp;
+							{publication.year}. &nbsp;
+							<span className={getStatusClass(publication.status[0])}>{publication.status}</span>
+							{publication.link && <a href={publication.link} target="_blank" rel="noopener noreferrer"> [Link] </a>}
 						</td>
 					</tr>
 				})}
@@ -71,4 +84,4 @@ const Publications = () => {
 	</>
 }
 
-export default Publications
+export default Publications;
