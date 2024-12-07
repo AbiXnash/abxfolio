9300b7588002ae656d3db422bf5a20a71d82d12d
AbiXnash <abixnash@outlook.com>
2024-12-07 22:26:59 +0530

    feat: implement Home page layout with CSS styling and profile image

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,28 +1,34 @@
-
+import './Home.css'
+import profile from '../../assets/profile.png'
 
 const Home = () => {
-	return <>
-		<h1>Home</h1>
-		<img alt="Abinash Selvarasu" />
-		<h2>
-			Hello World!
-		</h2>
-		<p>
-			Welcome to abxfolio, <span> I'm Abinash </span>
-		</p>
-		<p>
-			[ Name: Abinash Selvarasu, ]
-		</p>
-		<p>
-			[ Description: Welcome to abxfolio, folks! I'm a Full Stack Developer, Deep Learning Enthusiast
-			and I'm here to help you with your next project. ]
-		</p>
-		<p>
-			<span> Deep Learning </span>
-			<span> Full Stack Development </span>
-			<span> Python </span>
-		</p>
-	</>
+	return <section>
+		<div className='container'>
+			<div className='profile-view'>
+				<img src={profile} alt="Abinash Selvarasu" />
+			</div>
+			<div className='introduction'>
+				<h2>
+					Hello World!
+				</h2>
+				<p>
+					Welcome to abxfolio, <span> I'm Abinash </span>
+				</p>
+				<p>
+					[ Name: Abinash Selvarasu, ]
+				</p>
+				<p>
+					[ Description: Welcome to abxfolio, folks! I'm a Full Stack Developer, Deep Learning Enthusiast
+					and I'm here to help you with your next project. ]
+				</p>
+				<p>
+					<span> Deep Learning </span>
+					<span> Full Stack Development </span>
+					<span> Python </span>
+				</p>
+			</div>
+		</div>
+	</section>
 }
 
 export default Home
