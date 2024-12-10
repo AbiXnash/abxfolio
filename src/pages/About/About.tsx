6fa63db6611813bf2d6f79e49c36a042253d16ba
AbiXnash <abixnash@outlook.com>
2024-12-10 18:23:28 +0530

    feat: add jQuery dependency; implement circular progress cards in About page with responsive design

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -10,16 +10,68 @@ const About = () => {
 				While I'm not coding, I'm probably reading "The Alchemist" or convincing myself that I'm a good cook.
 				If you're here looking for brilliance, welcome aboard! If you're here to hire me, I'm flattered.
 			</p>
-			<div className="skills row">
-			<h1>Skillset</h1>
-			<ul className="skills-list">
-				<li>JavaScript - 90%</li>
-				<li>React - 85%</li>
-				<li>Node.js - 80%</li>
-				<li>Python - 75%</li>
-				<li>Machine Learning - 70%</li>
-			</ul>
-			</div>
+			<div className="circular_progress">
+  <div className="responsive-container-block big-container">
+    <div className="responsive-container-block container">
+      <p className="text-blk section-headtext">
+        Lorem ipsum dolor sit amet
+      </p>
+      <p className="text-blk section-subheadtext">
+        Sit amet massa vitae tortor condimentum lacinia quis vel eros. Est ullamcorper eget nulla facilisi etiam dignissim diam quis enim.
+      </p>
+      <div className="responsive-container-block">
+        <div className="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12">
+          <div className="card">
+            <div className="circlechart circle-chart container-block" data-percentage="95">
+            </div>
+            <p className="text-blk card-head">
+              Lorem ipsum
+            </p>
+            <p className="text-blk card-text">
+              Lorem ipsum dolor sit amet, consectetur adip iscing elit,
+            </p>
+          </div>
+        </div>
+        <div className="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12">
+          <div className="card">
+            <div className="circlechart circle-chart container-block" data-percentage="30">
+            </div>
+            <p className="text-blk card-head">
+              Lorem ipsum
+            </p>
+            <p className="text-blk card-text">
+              Lorem ipsum dolor sit amet, consectetur adip iscing elit,
+            </p>
+          </div>
+        </div>
+        <div className="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12">
+          <div className="card">
+            <div className="circlechart circle-chart container-block" data-percentage="60">
+            </div>
+            <p className="text-blk card-head">
+              Lorem ipsum
+            </p>
+            <p className="text-blk card-text">
+              Lorem ipsum dolor sit amet, consectetur adip iscing elit,
+            </p>
+          </div>
+        </div>
+        <div className="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12">
+          <div className="card">
+            <div className="circlechart circle-chart container-block" data-percentage="-50">
+            </div>
+            <p className="text-blk card-head">
+              Lorem ipsum
+            </p>
+            <p className="text-blk card-text">
+              Lorem ipsum dolor sit amet, consectetur adip iscing elit,
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 		</div>
 	</section>
 }
