08cc316580259f1396a5a91575790df2799f0034
AbiXnash <abixnash@outlook.com>
2024-12-12 17:53:45 +0530

    feat: update About page description for improved clarity and engagement

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
 		<div className="container row">
 			<h1>About</h1>
 			<p>
-				Hey there! I'm a final-year student pretending to have my life together while juggling code, cofee and deadlines.
+				Hey there! I'm a final-year student pretending to have my life together while juggling code, reading and deadlines.
 				I specialize in AI/ML and full stack developoment -- basically, I teach machines to be smart while I figure out life.
 				While I'm not coding, I'm probably reading "The Alchemist" or convincing myself that I'm a good cook.
 				If you're here looking for brilliance, welcome aboard! If you're here to hire me, I'm flattered.
