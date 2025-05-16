5310c4826ea5e352b2e8adec7189f4c190ddf1e5
Abinash <abixnash@gmail.com>
2025-05-16 23:22:55 +0530

    initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import altanoprettierResponse from '@altano/astro-prettier-response';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [altanoprettierResponse()]
+});
