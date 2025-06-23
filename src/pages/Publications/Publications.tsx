8cda17e421499c71e4d7a4053ec43c5a022ae627
Abinash <abixnash@gmail.com>
2025-06-23 22:43:48 +0530

    Publications update

--- src/pages/Publications/Publications.tsx
+++ src/pages/Publications/Publications.tsx
@@ -1,27 +1,32 @@
-import arrow from "../../assets/arrow.svg";
 import './Publications.scss'
 
 const Publications = () => {
     const publications: {
-        id: number;
         title: string;
         year: string;
         publisher: string;
     }[] = [
             {
-                id: 1,
                 title: "HATT-MLPNN: A Hybrid Approach for Cyber-Attack Detection in Industrial Control Systems Using MLPNN and Attention Mechanisms",
                 year: "2024",
                 publisher: "International Conference on Applications and Technologies in Information Security",
             },
             {
-                id: 2,
+                title: "HG-GENAPT: A Novel Hypergraph based Genetic Programming Approach for Multi-Stage Advanced Persistent Threat Detection",
+                year: "2024",
+                publisher: "Journal of Transformative Technologies and Sustainable Development",
+            },
+            {
                 title: "HGCNN-LSTM: A Data-Driven Approach for Cyber Attack Detection in Cyber-Physical Systems",
                 year: "2024",
                 publisher: "Springer Journals",
             },
             {
-                id: 3,
+                title: "HyGAN-AD: A Hypergraph-based GAN Framework for Cyberattack Detection in Critical Infrastructures",
+                year: "2024",
+                publisher: "Springer LNNS Journals",
+            },
+            {
                 title: "HyperCoar-DAM: A Robust Intrusion Detection System for Enhancing IIoT Networks Protection",
                 year: "2024",
                 publisher: "International Journal of Advances in Engineering and Applied Mathematics",
@@ -36,7 +41,7 @@ const Publications = () => {
                     {publications.map((publication, index) => {
                         return (
                             <tr key={index} className="publication-row">
-                                <td className="publication-id">[<em>{publication.id}</em>]</td>
+                                <td className="publication-id">[<em>{index + 1}</em>]</td>
                                 <td className="publication-details">
                                     <em>{publication.title}, </em>
                                     {publication.publisher}, &nbsp;
